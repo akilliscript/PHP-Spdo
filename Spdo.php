93303d5330126e4df7d90634a47b6c7641cf7969
Gürkan Biçer <gurkan@gurkanbicer.com>
2017-03-25 20:14:10 +0200

    Update Spdo.php

--- Spdo.php
+++ Spdo.php
@@ -6,7 +6,7 @@
  * @version 1.0
  * @license http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPL 3.0
  * @since 2015-08-27
- * @link https://github.com/gurkanbicer/CI-Spdo
+ * @link https://github.com/gurkanbicer/PHP-Spdo
  */
 
     require_once APPPATH . 'config/database.php'; // IF you want use this class on standard PHP projects, you must be removed the this line. 
